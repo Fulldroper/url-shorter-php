feb549e502e1883267fe3e0eaf2eb1804f2efa3d
Fulldroper <40297614+Fulldroper@users.noreply.github.com>
2019-04-24 11:30:35 +0300

    Update url-shorter-file-read.php

--- url-shorter-file-read.php
+++ url-shorter-file-read.php
@@ -62,19 +62,19 @@ function selfURL(){
 	while($arr[$counter-1]!= '/'){$counter--;}
 	for($i = 0;$i<=$counter-1;$i++){$result = $result.$arr[$i];}
 	return $result; */
-	return $_SERVER[HTTP_HOST].$_SERVER[PHP_SELF];
+	return substr($_SERVER[HTTP_HOST].$_SERVER[PHP_SELF],0,-9);
 }
 if($_GET['show']=="all"){
 	showList($filename);
 }
 if($_GET['id']==null){
-echo'<head><title>URL Shorter</title></head><body bgcolor="#202225"><form style="margin-top:20%;margin-left:35%;" method="post" action="#"><h1 style="color:white;">URL Shorter by '.$_SERVER[HTTP_HOST].'</h1><p><input style="margin-right:10px;padding-left:1%;border-radius:10px 10px 10px 10px;width:25%;" type="text" placeholder="LongURL" name="url" /><input style="margin-top:20px;padding-left:3px;width:80px;margin-right:10px;border-radius:10px 10px 10px 10px;" type="text" value='.CC_rand().' placeholder="ShortURL" name="id" /><input style="margin-top:20px;padding:5px;color:#202225;border:none;background-color:#474a4f;border-radius:10px;" type="submit" value=GENERATE /></p></form></body>';
+echo'<head><title>URL Shorter</title></head><body bgcolor="#202225"><form style="margin-top:20%;margin-left:35%;" method="post" action="#"><h1 style="color:#515151;">URL Shorter by '.$_SERVER[HTTP_HOST].'</h1><p><input style="margin-right:10px;padding-left:1%;border-radius:10px 10px 10px 10px;width:25%;" type="text" placeholder="LongURL" name="url" /><input style="margin-top:20px;padding-left:3px;width:80px;margin-right:10px;border-radius:10px 10px 10px 10px;" type="text" value='.CC_rand().' placeholder="ShortURL" name="id" /><input style="margin-top:20px;padding:5px;color:#a8927d;border:none;background-color:#32363c;border-radius:10px;" type="submit" value=GENERATE /></p></form></body>';
 }
 if($_POST['id']!=null and $_POST['url']!=null){
 	$obj=findId($_POST['id'],$filename);
 	if($obj==false){
 		addReq($_POST['url'],$_POST['id'],$filename);
-		echo'<p style=padding:5px;margin-left:35%;width:320px;background:#32363c;border-radius:10px 10px 10px 10px;padding-bottom:10px;padding-left:15px;padding-right:15px;padding-top:10px;font-size:14px;font-color:#839496;color:#839496;><a href=http://'.selfURL().'?id='.$_POST['id'].' style=color:#6d6e71;text-decoration:none;left:auto; >http://'.selfURL().'?id='.$_POST['id'].'</a></p>';
+		echo'<p style=text-align:center;padding:5px;margin-left:35%;width:max-content;background:#32363c;border-radius:10px 10px 10px 10px;padding-bottom:10px;padding-left:15px;padding-right:15px;padding-top:10px;font-size:14px;font-color:#839496;color:#839496;><a style="border-radius: 10px 0 0 10px;padding:5px;margin-right:5px;margin-left:-5px;background-color:#515151;">URL</a><a href=http://'.selfURL().$_POST['id'].' style=color:#6d6e71;text-decoration:none;left:auto; >'.selfURL().$_POST['id'].'</a></p>';
 	}else{
 		echo'<head><title>URL Shorter</title></head><body bgcolor=#202225 ><form style="margin-top:20%;margin-left:35%;" method=post action=# ><h1 style="color:white;">URL Shorter by '.$_SERVER[HTTP_HOST].'</h1><b style=color:red; > ERROR 500:12 - Link reserved</b><p><input style=margin-right:10px;padding-left:1%;border-radius:10px 10px 10px 10px;width:25%; type=text placeholder="LongURL" name=url /><input style=margin-top:20px;padding-left:3px;width:80px;margin-right:10px;border-radius:10px 10px 10px 10px; type=text value='.CC_rand().' placeholder="ShortURL" name=id /><input style=margin-top:20px;padding:5px;color:#202225;border:none;background-color:#474a4f;border-radius:10px; type=submit value=GENERATE /></p></form></body>';
 	}
